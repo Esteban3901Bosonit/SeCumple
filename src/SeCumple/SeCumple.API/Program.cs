a6e88838b69beee18ac779e2a0c3b3f6d0343cf9
Jeyson Esteban Parra Bedoya <COX0000732@prosegur.com>
2024-10-30 12:44:03 -0500

    Enable swagger

--- src/SeCumple/SeCumple.API/Program.cs
+++ src/SeCumple/SeCumple.API/Program.cs
@@ -21,11 +21,12 @@ builder.Services.AddInfrastructureDependencies(builder.Configuration);
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+// if (app.Environment.IsDevelopment())
+// {
+// }
+
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
