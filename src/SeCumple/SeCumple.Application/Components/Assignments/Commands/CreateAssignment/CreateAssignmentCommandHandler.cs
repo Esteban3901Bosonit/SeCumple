991d16fcf4335273ff06c04622340cd1e67ccc43
Jeyson Esteban Parra Bedoya <COX0000732@prosegur.com>
2024-10-30 19:03:47 -0500

    Modified Assigment process to multiple plans and sectors

--- src/SeCumple/SeCumple.Application/Components/Assignments/Commands/CreateAssignment/CreateAssignmentCommandHandler.cs
+++ src/SeCumple/SeCumple.Application/Components/Assignments/Commands/CreateAssignment/CreateAssignmentCommandHandler.cs
@@ -7,42 +7,57 @@ using SeCumple.Infrastructure.Persistence.Interfaces;
 namespace SeCumple.Application.Components.Assignments.Commands.CreateAssignment;
 
 public class CreateAssignmentCommandHandler(IUnitOfWork unitOfWork)
-    : IRequestHandler<CreateAssignmentCommand, ProcessResult<AssignmentResponse>>
+    : IRequestHandler<CreateAssignmentCommand, ProcessResult<List<AssignmentResponse>>>
 {
-    public async Task<ProcessResult<AssignmentResponse>> Handle(CreateAssignmentCommand request,
+    public async Task<ProcessResult<List<AssignmentResponse>>> Handle(CreateAssignmentCommand request,
         CancellationToken cancellationToken)
     {
-        var assignment = new Assignment
+        var response = new ProcessResult<List<AssignmentResponse>>
         {
-            UserId = request.iCodUsuario,
-            RoleId = request.iRol,
-            PlanId = request.iDetPlanCumplimiento,
-            SectorId = request.iMaeSector,
-            Username = request.cUserName,
-            Status = '1',
-            CreatedBy = request.iCodUsuarioRegistro
+            Data = new List<AssignmentResponse>()
         };
 
-        await unitOfWork.Repository<Assignment>().AddAsync(assignment);
+        var assignmentTasks = new List<Task>();
 
-        return new ProcessResult<AssignmentResponse>
+        foreach (var planId in request.iDetPlanCumplimiento)
         {
-            Data = new AssignmentResponse
+            foreach (var sectorId in request.iMaeSector)
             {
-                iMovAsignacion = assignment.Id,
-                iMaeSector = assignment.SectorId,
-                iRol = assignment.RoleId,
-                iDetPlanCumplimiento = assignment.PlanId,
-                cEstado = assignment.Status == '1' ? "ACTIVO" : "INACTIVO"
+                var assignment = new Assignment
+                {
+                    UserId = request.iCodUsuario,
+                    RoleId = request.iRol,
+                    PlanId = planId,
+                    SectorId = sectorId,
+                    Username = request.cUserName,
+                    Status = '1',
+                    CreatedBy = request.iCodUsuarioRegistro
+                };
+
+                assignmentTasks.Add(Task.Run(async () =>
+                {
+                    await unitOfWork.Repository<Assignment>().AddAsync(assignment);
+                    response.Data.Add(new AssignmentResponse
+                    {
+                        iDetPlanCumplimiento = assignment.PlanId,
+                        iMaeSector = assignment.SectorId,
+                        iRol = assignment.RoleId,
+                        iCodUsuario = assignment.UserId,
+                        cUserName = assignment.Username
+                    });
+                }));
             }
-        };
+        }
+        await Task.WhenAll(assignmentTasks);
+
+        return response;
     }
 }
 
-public class CreateAssignmentCommand : IRequest<ProcessResult<AssignmentResponse>>
+public class CreateAssignmentCommand : IRequest<ProcessResult<List<AssignmentResponse>>>
 {
-    public int iDetPlanCumplimiento { get; set; }
-    public int iMaeSector { get; set; }
+    public int[] iDetPlanCumplimiento { get; set; }
+    public int[] iMaeSector { get; set; }
     public int iCodUsuario { get; set; }
     public int iRol { get; set; }
     public int iCodUsuarioRegistro { get; set; }
